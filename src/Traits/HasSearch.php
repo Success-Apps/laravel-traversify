992b06e9ddd3743fdaa65112d0c7372672776cd7
Marc Locchi <88011207+TheRealPapa@users.noreply.github.com>
2022-01-12 01:15:31 +1000

    Fixed bug that broke query when search was combined with multi level relations

--- src/Traits/HasSearch.php
+++ src/Traits/HasSearch.php
@@ -5,7 +5,6 @@ use Exception;
 use RuntimeException;
 use Illuminate\Support\Str;
 use InvalidArgumentException;
-use Illuminate\Support\Facades\Log;
 use Kirschbaum\PowerJoins\PowerJoins;
 use Illuminate\Database\Eloquent\Builder;
 
@@ -37,13 +36,14 @@ trait HasSearch
         $key = $this->connection ?: config('database.default');
 
         if(config('database.connections.' . $key . '.driver') == 'pgsql') {
-
             $this->like = 'ILIKE';
         }
 
+        $sortedSearches = $this->sortSearches($this->search);
+
         $columns = [];
 
-        foreach($searches as $searchable) {
+        foreach($sortedSearches as $searchable) {
 
             $searchables = explode('.', $searchable);
 
@@ -53,32 +53,35 @@ trait HasSearch
 
                 $model = new self;
 
-                foreach($searchables as $relationship) {
+                foreach($searchables as $key => $relationship) {
+
                     $model = $model->$relationship()->getRelated();
+
+                    $tableName = $model->getTable();
+
+                    if ($key === array_key_last($searchables)) {
+
+                        $column = $this->prepSearchId($tableName, $searchColumn);
+
+                        array_push($columns, $column);
+                    }
+
+                    $tableJoins = collect($query->getQuery()->joins)->pluck('table');
+
+                    [$joined, $cleanJoinList] = $this->isJoined($tableJoins, $tableName);
+
+                    if(!$joined) {
+
+                        $query->leftJoinRelationship(implode('.', $searchables));
+                    }
                 }
-
-                $tableName = $model->getTable();
-
-                $alias = $tableName;
-
-                if(!collect($query->getQuery()->joins)->pluck('table')->contains($tableName)) {
-
-                    $alias = count($searchables) === 1 ? strtolower($searchables[0]) : $tableName;
-
-                    $query->leftJoinRelationship(implode('.', $searchables), $alias);
-                }
-
-                array_push($columns, "$alias.$searchColumn");
 
             } else {
 
                 $tableName = $this->getTable();
 
-                $column = $tableName.'.'.$searchColumn;
-                if ($searchColumn == 'id') {
-                    $prefix =  strtoupper(substr($tableName, 0, 1));
-                    $column = "'".$prefix."'".', '.$tableName.'.'.$searchColumn;
-                }
+                $column = $this->prepSearchId($tableName, $searchColumn);
+
                 array_push($columns, $column);
             }
         }
@@ -87,4 +90,87 @@ trait HasSearch
 
         return $query->whereRaw("CONCAT_WS(' ', {$columns}) {$this->like} ?", "%{$keyword}%");
     }
+
+    /**
+     * Sort Searches
+     *
+     * @param $searches
+     * @return array
+     */
+    private function sortSearches($searches) {
+
+        $counted = [];
+        $i = 0;
+
+        foreach ($searches as $search) {
+            array_push($counted, ['index'=> $i, 'count' => substr_count($search, '.')]);
+            $i++;
+        }
+
+        array_multisort(array_column($counted, 'count'), SORT_DESC, $counted);
+
+        $sorted = [];
+
+        foreach ($counted as $count) {
+            array_push($sorted, $searches[$count['index']]);
+        }
+
+        return  $sorted;
+    }
+
+    /**
+     * Check if query has Join
+     *
+     * @param $tableJoins
+     * @param $tableName
+     * @return array
+     */
+    private function isJoined($tableJoins, $tableName) {
+
+        $newJoins = $tableJoins->map(function ($join) {
+
+            if (str_contains($join, ' as ')) {
+                $explode = explode(' as ', $join);
+                $join = $explode[0];
+            }
+
+            return $join;
+
+        });
+
+        if ($newJoins->contains($tableName)) {
+            return [
+                true,
+                $newJoins->unique()
+            ];
+        }
+
+        return [
+            false,
+            $newJoins->unique()
+        ];
+    }
+
+    /**
+     * Setup ID Search
+     *
+     * @param $tableName
+     * @param $searchColumn
+     * @return string
+     */
+    private function prepSearchId($tableName, $searchColumn) {
+
+        $column = $tableName.'.'.$searchColumn;
+
+        if ($searchColumn == 'id') {
+
+            $prefix =  strtoupper(substr($tableName, 0, 1));
+
+            $column = "'".$prefix."'".', '.$tableName.'.'.$searchColumn;
+        }
+
+        return $column;
+    }
+
 }
+
