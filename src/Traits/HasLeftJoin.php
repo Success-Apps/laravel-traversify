0f726eab5539e9ad44d7bb4fb618fa971598eacc
Kevin King <keinstah@gmail.com>
2024-03-19 07:17:30 +0800

    Revert "fixes to exception on HasLeftJoin"
    This reverts commit 6925a4afabeca750ead8519ea6a5ef1a9b294270.

--- src/Traits/HasLeftJoin.php
+++ src/Traits/HasLeftJoin.php
@@ -495,29 +495,21 @@ trait HasLeftJoin
         $alias = null;
 
         foreach($tableJoins as $join) {
-            if (is_string($join)) {
-                $join = strtolower($join);
+            $join = strtolower($join);
 
-                if (str_contains($join, ' as ')) {
+            if (str_contains($join, ' as ')) {
 
-                    $explode = explode(' as ', $join);
-                    $join = $explode[0];
-                    $alias = $explode[1];
+                $explode = explode(' as ', $join);
+                $joinTable = $explode[0];
+                $alias = $explode[1];
 
-                    if ($join == $tableName) {
-                        return $alias;
-                    }
+                if ($joinTable == $tableName) {
+                    return $alias;
                 }
+            }
 
-                if ($join == $tableName) {
-                    return $tableName;
-                }
-            } else if ($join instanceof Expression) {
-                $expression = $join->getValue($this->getGrammar());
-
-                if (str_contains($expression, "from `$tableName`")) {
-                    return true;
-                }
+            if ($join == $tableName) {
+                return $tableName;
             }
         };
 
